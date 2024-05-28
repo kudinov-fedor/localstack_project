50d56ca9b5ed0a8c1d5769bf0b2b7d0ccfcf6f9a
fkudinov <fkudinov@modeln.com>
2024-05-28 12:19:41 +0300

    add workarround for windows

--- get_layer_packages.sh
+++ get_layer_packages.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Workaround for Docker for Windows in Git Bash.
+#docker()
+#{
+#    (export MSYS_NO_PATHCONV=1; "docker.exe" "$@")
+#}
+
+
 export PKG_DIR="lambda_layer/python/lib/python3.8/site-packages"
 
 rm -rf ${PKG_DIR} && mkdir -p ${PKG_DIR}
